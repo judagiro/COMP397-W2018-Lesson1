2cc161e8ce14b4c69cfb913927c5b56bb8a5b375
Julian Giraldo <judagiro@gmail.com>
2018-03-01 17:39:38 -0500

    added npm support

---
+++ Scripts/core/game.js
@@ -0,0 +1,23 @@
+// IIFE - Immediately Invoked Function Expression
+(function(){
+    
+    function Init(){
+        console.log("Initialization Started");
+    }
+
+    function Start(){
+
+    }
+
+    function Update(){
+
+    }
+
+    function Main() {
+        
+    }
+
+    window.onload = Init;
+
+
+})();
